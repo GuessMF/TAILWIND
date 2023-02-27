fa78a70e9d8fa26ba168cf47e72190733192234e
Sergey <Segas95@yandex.ru>
2023-02-27 14:29:35 +0300

    ffw

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-var message = "Hello World";
-console.log(message);
+var newPasswodrd = "";
+var genButton = document.querySelector("#genButton");
+var generatedPass = document.querySelector("#generatedPass");
+var randNum;
+function newRandom() {
+    randNum = Math.random() * 100;
+}
+genButton.addEventListener("click", function () {
+    //   console.log(randNum);
+    console.log(generatedPass.textContent);
+    newRandom();
+    generatedPass.textContent = randNum;
+});
--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-let message: string = "Hello World";
-console.log(message);
+let newPasswodrd: string = "";
+const genButton: any = document.querySelector("#genButton");
+let generatedPass: any = document.querySelector("#generatedPass");
+let randNum: number;
+
+function newRandom() {
+  randNum = Math.random() * 100;
+}
+genButton.addEventListener("click", () => {
+  //   console.log(randNum);
+  console.log(generatedPass.textContent);
+  newRandom();
+  generatedPass.textContent = randNum;
+});
